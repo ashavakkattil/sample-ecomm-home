802d19529609e91960b2d89a2e024033018df2f4
Asha <asha.vakkattil@gmail.com>
2020-05-05 19:29:43 +0530

    Page Footer
    Pending: Subscribe button

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,32 +1,32 @@
 <template>
   <div class="q-ma-xl q-py-xl">
     <div class="row">
-      <div class="col-sm-3 col-xs-12">
-        <div class="text-h6 text-uppercase q-mb-lg">Company</div>
-        <div>About</div>
-        <div>Press</div>
-        <div>Blog</div>
+      <div class="col-sm-3 col-xs-12 q-mb-lg">
+        <div class="text-h6 text-uppercase q-mb-md">Company</div>
+        <div class="text-body1 q-mb-md">About</div>
+        <div class="text-body1 q-mb-md">Press</div>
+        <div class="text-body1 q-mb-md">Blog</div>
       </div>
-      <div class="col-sm-3 col-xs-12">
-        <div class="text-h6 text-uppercase q-mb-lg">Partners</div>
-        <div>About</div>
-        <div>Press</div>
-        <div>Blog</div>
+      <div class="col-sm-3 col-xs-12 q-mb-lg">
+        <div class="text-h6 text-uppercase q-mb-md">Partners</div>
+        <div class="text-body1 q-mb-md">Advertise</div>
+        <div class="text-body1 q-mb-md">Affilate</div>
+        <div class="text-body1 q-mb-md">Career</div>
       </div>
-      <div class="col-sm-3 col-xs-12">
-        <div class="text-h6 text-uppercase q-mb-lg">Support</div>
-        <div>About</div>
-        <div>Press</div>
-        <div>Blog</div>
+      <div class="col-sm-3 col-xs-12 q-mb-lg">
+        <div class="text-h6 text-uppercase q-mb-md">Support</div>
+        <div class="text-body1 q-mb-md">Help Centre</div>
+        <div class="text-body1 q-mb-md">Contact Us</div>
+        <div class="text-body1 q-mb-md">Terms & Privacy</div>
       </div>
-      <div class="col-sm-3 col-xs-12">
-        <div class="text-h6 text-uppercase q-mb-lg">Subscribe</div>
-        <q-input outlined></q-input>
+      <div class="col-sm-3 col-xs-12 q-mb-lg">
+        <div class="text-h6 text-uppercase q-mb-md">Subscribe</div>
+        <q-input outlined placeholder="Email Address"></q-input>
         <div class="q-gutter-md q-mt-lg">
           <q-icon name="mdi-facebook" size="sm"></q-icon>
-          <q-icon name="mdi-facebook" size="sm"></q-icon>
-          <q-icon name="mdi-facebook" size="sm"></q-icon>
-          <q-icon name="mdi-facebook" size="sm"></q-icon>
+          <q-icon name="mdi-instagram" size="sm"></q-icon>
+          <q-icon name="mdi-twitter" size="sm"></q-icon>
+          <q-icon name="mdi-youtube" size="sm"></q-icon>
         </div>
       </div>
     </div>
