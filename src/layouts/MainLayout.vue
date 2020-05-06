70bbc2b09f28834fc5bcc842effd491b417da5da
Asha <asha.vakkattil@gmail.com>
2020-05-06 12:45:21 +0530

    Footer responsiveness

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,19 +2,11 @@
   <q-layout view="lHh Lpr rff">
     <q-header elevated></q-header>
     <q-footer class="app-footer q-mb-lg">
-      <div class="row" v-if="$q.screen.xs">
-        <div class="col-12 text-center">
+      <div class="row ">
+        <div class="col-xs-12 col-md-6" :class="$q.screen.gt.xs ? 'text-right': 'text-center'">
           <q-img src="../assets/quasar-logo-full.svg" height="100px" width="100px" contain></q-img>
         </div>
-        <div class="col-12">
-          <div class="text-body1 text-capitalize text-center">@2020 Tinse | All rights reserved</div>
-        </div>
-      </div>
-      <div class="row" v-else>
-        <div class="col-6 text-right">
-          <q-img src="../assets/quasar-logo-full.svg" height="100px" width="100px" contain></q-img>
-        </div>
-        <div class="col-6 text-left" style="align-self: center;">
+        <div class="col-xs-12 col-md-6" :class="$q.screen.gt.xs ? 'text-left': 'text-center'" style="align-self: center">
           <div class="text-body1 text-capitalize">@2020 Tinse | All rights reserved</div>
         </div>
       </div>
